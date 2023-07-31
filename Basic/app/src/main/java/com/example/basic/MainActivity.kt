b3b4c22a8936add0c37edc7b2cac406a088c04e7
jihun.bang <jihun.bang@sk.com>
2023-07-31 14:18:51 +0900

    상태 호이스팅

--- Basic/app/src/main/java/com/example/basic/MainActivity.kt
+++ Basic/app/src/main/java/com/example/basic/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,8 +14,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -35,13 +36,53 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(
+fun MyApp(modifier: Modifier = Modifier) {
+    var shouldShowSplash by remember { mutableStateOf(true) }
+
+    Surface(modifier = modifier) {
+        if (shouldShowSplash) {
+            SplashScreen(
+                onContinue = { shouldShowSplash = false }
+            )
+        } else {
+            Greetings()
+        }
+    }
+}
+
+
+@Composable
+fun SplashScreen(
+    modifier: Modifier = Modifier,
+    onContinue: () -> Unit
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Welcome to the Compose!")
+        ElevatedButton(
+            modifier = modifier.padding(24.dp),
+            onClick = onContinue
+        ) {
+            Text(text = "Continue")
+        }
+    }
+}
+
+
+@Composable
+fun Greetings(
     modifier: Modifier = Modifier,
     names: List<String> = listOf("World", "Compose")
 ) {
     Column(modifier = modifier.padding(vertical = 4.dp)) {
         for (name in names) {
-            Greeting(name = name, modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp))
+            Greeting(
+                name = name,
+                modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+            )
         }
     }
 }
