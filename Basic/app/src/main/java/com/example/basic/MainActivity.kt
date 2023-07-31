bc70558b8cb19b1c1b090dea1c9239f1ed825a23
jihun.bang <jihun.bang@sk.com>
2023-07-31 14:04:05 +0900

    상태 처리

--- Basic/app/src/main/java/com/example/basic/MainActivity.kt
+++ Basic/app/src/main/java/com/example/basic/MainActivity.kt
@@ -15,6 +15,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -39,29 +41,34 @@ fun MyApp(
 ) {
     Column(modifier = modifier.padding(vertical = 4.dp)) {
         for (name in names) {
-            Box(modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
-                Greeting(name = name)
-            }
+            Greeting(name = name, modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp))
         }
     }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = MaterialTheme.colorScheme.primary) {
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+
+
+    Surface(color = MaterialTheme.colorScheme.primary, modifier = modifier) {
         Row(
             modifier = Modifier
                 .padding(24.dp)
                 .fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
+            verticalAlignment = Alignment.Top
         ) {
-            Column {
+            Column(modifier = Modifier.padding(bottom = extraPadding)) {
                 Text(text = "Hello,")
                 Text(text = name)
             }
-            ElevatedButton(onClick = { /*TODO*/ }) {
-                Text(text = "Show more")
+            ElevatedButton(
+                onClick = {
+                    expanded.value = !expanded.value
+                }) {
+                Text(text = "Show ${if (expanded.value) "Less" else "More"}")
             }
         }
     }
