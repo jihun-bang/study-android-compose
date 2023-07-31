8cfda9d9b082bee5536795c4de2cac83264d4959
jihun.bang <jihun.bang@sk.com>
2023-07-31 15:01:43 +0900

    테마 적용

--- Basic/app/src/main/java/com/example/basic/MainActivity.kt
+++ Basic/app/src/main/java/com/example/basic/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.basic
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -23,6 +24,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.basic.ui.theme.BasicTheme
@@ -106,7 +108,12 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         ) {
             Column(modifier = Modifier.padding(bottom = extraPadding)) {
                 Text(text = "Hello,")
-                Text(text = name)
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.headlineMedium.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
             }
             ElevatedButton(
                 onClick = {
@@ -118,10 +125,16 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     }
 }
 
-@Preview(showBackground = true, name = "Text Preview", widthDp = 320)
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "Dark Mode"
+)
+@Preview(showBackground = true, name = "Light Mode", widthDp = 320)
 @Composable
 fun GreetingPreview() {
     BasicTheme {
-        MyApp()
+        Greetings()
     }
 }
