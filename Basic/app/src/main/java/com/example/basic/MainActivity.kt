5745a2dd44eb3d90db39688aa16875a874d09987
jihun.bang <jihun.bang@sk.com>
2023-07-31 14:22:13 +0900

    Lazy List

--- Basic/app/src/main/java/com/example/basic/MainActivity.kt
+++ Basic/app/src/main/java/com/example/basic/MainActivity.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -75,10 +77,10 @@ fun SplashScreen(
 @Composable
 fun Greetings(
     modifier: Modifier = Modifier,
-    names: List<String> = listOf("World", "Compose")
+    names: List<String> = List(1000) { "$it" }
 ) {
-    Column(modifier = modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
             Greeting(
                 name = name,
                 modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
